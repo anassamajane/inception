36ccfd79956d8f1f387e7f0f0c7d28d5fb55201c
osboxes.org <anass.amajane2001@gmail.com>
2026-05-03 12:35:49 +0100

    fully automate install

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -4,22 +4,15 @@ set -e
 #set -x
 
 echo "Waiting for MariaDB..."
-MAX_TRIES=30
-COUNT=0
 until mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SELECT 1;" "$MYSQL_DATABASE" 2>&1; do
-    COUNT=$((COUNT+1))
-    if [ $COUNT -ge $MAX_TRIES ]; then
-        echo "MariaDB never became ready, giving up"
-        exit 1
-    fi
-    echo "Attempt $COUNT/$MAX_TRIES - retrying in 3s..."
-    sleep 3
+    sleep 1
 done
 
 echo "MariaDB is fully ready!"
 
 cd /var/www/html/wordpress
 
+# create config if not exists
 if [ ! -f wp-config.php ]; then
     cp wp-config-sample.php wp-config.php
 
@@ -27,6 +20,17 @@ if [ ! -f wp-config.php ]; then
     sed -i "s/username_here/$MYSQL_USER/" wp-config.php
     sed -i "s/password_here/$MYSQL_PASSWORD/" wp-config.php
     sed -i "s/localhost/$MYSQL_HOST/" wp-config.php
+fi
+
+# install wordpress if not installed
+if ! wp core is-installed --allow-root;then
+	wp core install \
+		--url="$WP_URL" \
+		--title="$WP_TITLE" \
+		--admin_user="$WP_ADMIN_USER" \
+		--admin_password="$WP_ADMIN_PASSWORD" \
+		--admin_email="$WP_ADMIN_EMAIL" \
+		--allow-root
 fi
 
 echo "Starting PHP-FPM..."
