2ca568a2e1efa106500c2c9fb0f51f4bb4d1d3e7
osboxes.org <anass.amajane2001@gmail.com>
2026-05-06 09:05:16 +0100

    Add root password

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 #start Mariadb in background
 mysqld_safe --user=mysql &
 
@@ -9,17 +12,19 @@ until mysqladmin ping --silent; do
 done
 
 # check if database exists
-if ! mysql -e "USE $MYSQL_DATABASE;" 2>/dev/null; then
-	echo "Initializing database..."
-
-	mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
-	mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-	mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-	mysql -e "FLUSH PRIVILEGES;"
+if ! mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "USE $MYSQL_DATABASE;" 2>/dev/null; then
+    # first run - root has no password yet, init everything
+    mysql <<EOF
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+EOF
 fi
 
 # restart MariaDB
-mysqladmin -u root shutdown
+mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
 
 # Start MariaDB
 exec mysqld --user=mysql
